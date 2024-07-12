dd538323d97367109a2a0c509fa136a22a1b48e4
DexA1948 <devkotadexant@gmail.com>
2024-07-12 21:15:32 +0545

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Inter({
+    subsets: ['latin'],
+    weight: ['400', '700']
+});
+
+// @import url('https://fonts.googleapis.com/css2?family=Lusitana:wght@400;700&display=swap');
